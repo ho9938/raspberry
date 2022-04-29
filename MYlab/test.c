fb2cf2498b971b89fcabb831a904b1938f6e4d8e
Yoon Changho <ho9938@unist.ac.kr>
2022-04-29 08:15:13 +0100

    MYlab: overflow test

--- MYlab/test.c
+++ MYlab/test.c
@@ -2,16 +2,10 @@
 
 int main()
 {
-  int abs = 127;
-  unsigned exp = 0;
-  int i = 0;
-  while (i < 8)
-  {
-        exp = exp + ((abs % 2) << i);
-		printf("exp: %x\n", exp);
-        abs = abs / 2;
-		printf("abs: %d\n", abs);
-        i = i + 1;
-  }
-  printf("%x\n", exp);
+	int a = 1 << 30;
+
+	printf ("%d\n", a);
+	printf ("%d\n", a*2);
+
+	return 0;
 }
